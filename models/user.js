4c3400f79eb2a5c22ca00367ab348dc0f9d3e30c
Adam Myers <adammyers002@gmail.com>
2026-01-13 14:51:12 -0600

    add index, new and create routes

--- models/user.js
+++ models/user.js
@@ -1,5 +1,26 @@
 const mongoose = require("mongoose");
 
+const applicationSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  notes: {
+    type: String,
+  },
+  postingLink: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ["interested", "applied", "interviewing", "rejected", "accepted"],
+  },
+});
+
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
@@ -9,14 +30,16 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  applications: [applicationSchema],
 });
 
 const User = mongoose.model("User", userSchema);
 
-userSchema.pre('save', function (next) {
-  const docToBeSaved = this
+userSchema.pre("save", function (next) {
+  const docToBeSaved = this;
   if (docToBeSaved.username) {
-    docToBeSaved.username = docToBeSaved.username[0].toUpperCase() + docToBeSaved.username.slice(1);
+    docToBeSaved.username =
+      docToBeSaved.username[0].toUpperCase() + docToBeSaved.username.slice(1);
   }
   next();
 });
--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const morgan = require("morgan");
 const methodOverride = require("method-override");
 const authRoutes = require("./controllers/auth");
 const session = require("express-session");
-const MongoStore = require('connect-mongo')
+const MongoStore = require("connect-mongo");
+const isSignedIn = require("./middleware/is-signed-in");
+const passUserToView = require("./middleware/pass-user-to-view");
+const applicationsController = require("./controllers/application.js");
 
 // Middlewares
 require("./db/connection");
@@ -19,35 +22,26 @@ app.use(
     resave: false,
     saveUninitialized: true,
     store: MongoStore.create({
-        mongoUrl: process.env.MONGODB_URI
+      mongoUrl: process.env.MONGODB_URI,
     }),
     cookie: {
-        maxAge: 1000 * 60 * 60 * 24
-    }
-
+      maxAge: 1000 * 60 * 60 * 24,
+    },
   })
 );
 
 // Routes
+app.use(passUserToView);
+
 app.get("/", (req, res) => {
   res.render("index.ejs", {
     user: req.session.user,
   });
 });
+
 app.use("/auth", authRoutes);
-
-// Routes below this you must be signed in
-app.use((req, res, next) => {
-  if (req.session.user) {
-    next();
-  } else {
-    res.redirect("/");
-  }
-});
-
-app.get("/vip-lounge", (req, res) => {
-  res.send(`Welcome to the party ${req.session.user.username}`);
-});
+app.use(isSignedIn);
+app.use("/users/:userId/applications", applicationsController);
 
 app.listen(PORT, () => {
   console.log("This ship sailing on port", PORT);
