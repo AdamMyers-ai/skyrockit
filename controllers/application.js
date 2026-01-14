4892df357e192336975d00d2f00e348a9eef3f2d
Adam Myers <adammyers002@gmail.com>
2026-01-14 10:24:52 -0600

    add edit route and ejs file

--- controllers/application.js
+++ controllers/application.js
@@ -27,6 +27,29 @@ router.get("/new", async (req, res) => {
   res.render("applications/new.ejs");
 });
 
+// Update
+router.put("/:applicationId", async (req, res) => {
+  try {
+    // Find the user from req.session
+    const currentUser = await User.findById(req.session.user._id);
+    // Find the current application from the id supplied by req.params
+    const application = currentUser.applications.id(req.params.applicationId);
+    // Use the Mongoose .set() method
+    // this method updates the current application to reflect the new form
+    // data on `req.body`
+    application.set(req.body);
+    // Save the current user
+    await currentUser.save();
+    // Redirect back to the show view of the current application
+    res.redirect(
+      `/users/${currentUser._id}/applications/${req.params.applicationId}`
+    );
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
+
 // Delete
 router.delete("/:applicationId", async (req, res) => {
   try {
@@ -57,6 +80,24 @@ router.post("/", async (req, res) => {
     await currentUser.save();
     // Redirect back to the applications index view
     res.redirect(`/users/${currentUser._id}/applications`);
+  } catch (error) {
+    // If any errors, log them and redirect back home
+    console.log(error);
+    res.redirect("/");
+  }
+});
+
+// Edit
+router.get("/:applicationId/edit", async (req, res) => {
+  try {
+    // Look up the user from req.session
+    const currentUser = await User.findById(req.session.user._id);
+    // Find the application by the applicationId supplied from req.params
+    const application = currentUser.applications.id(req.params.applicationId);
+    // Render the show view, passing the application data in the context object
+    res.render("applications/edit.ejs", {
+      application: application,
+    });
   } catch (error) {
     // If any errors, log them and redirect back home
     console.log(error);
